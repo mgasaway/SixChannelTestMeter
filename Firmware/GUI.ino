04cd790fb2160891970d7303bb5c4310c9ecf9bf
mgasaway <mgazaway07@gmail.com>
2013-06-04 18:18:42 -0300

    Update GUI.ino

--- Firmware/GUI.ino
+++ Firmware/GUI.ino
@@ -1,6 +1,28 @@
 #include "GUI.h"
+#include <I2C.h>
 
-void clearSerial(){
+void initializeSerial(){
+  //initialize serial communication with computer
+  Serial.begin(19200);
+
+  //I2C HERE
   
+}
+
+void uploadResults(){
+  int i, j;
+  String toSend;
+  
+  for(i=0; i<TOTAL_MEASUREMENTS; i++){
+    toSend="";
+    
+    for(j=0; j<6; j++){
+      toSend=toSend+"ch"+j+":"+Channel[j].rawMeasurement[i]+",";
+    }
+    toSend=toSend+";";
+    
+    Serial.println(toSend);
+    delay(10);
+  }
   
 }
