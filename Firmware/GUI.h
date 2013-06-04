ba41d0abf29b992270698ec4ee5c3102dbf49d5d
mgasaway <mgazaway07@gmail.com>
2013-06-04 18:18:18 -0300

    Update GUI.h

--- Firmware/GUI.h
+++ Firmware/GUI.h
@@ -1,9 +1,12 @@
 #ifndef GUI_H
 #define GUI_H
 
+#include "Firmware.h"
+
 //Use Processing to provide GUI
 //import processing.serial.*;
 
-void clearSerial();
+void initializeSerial();
+void uploadResults();
 
 #endif
