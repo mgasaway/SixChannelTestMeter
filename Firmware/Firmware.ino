98aa8d0f554f191089203242acade947a0e7c7a4
mgasaway <mgazaway07@gmail.com>
2013-06-04 18:17:04 -0300

    Update Firmware.ino

--- Firmware/Firmware.ino
+++ Firmware/Firmware.ino
@@ -15,9 +15,11 @@ void setup() {
   pinMode(FILL_DETECTION_CH2, INPUT);
   
   //no need to declare ADC pins as input/output
+  //however we need to declare that we are using an external analog reference.
+  //analogReference(EXTERNAL);
   
-  //initialize serial communication with computer
-  Serial.begin(19200);
+  //initialize serial/I2C
+  initializeSerial();
   
   //initialize variables
   for(i=0; i < CHANNELS_IN_METER; i++){
@@ -27,13 +29,35 @@ void setup() {
 
 void loop() {
   //waitForBlood();
-  //for(i=0; i<10000; i++){
+  //for(i=0; i<150; i++){
   //  measureAllChannels();
   //}  
   // print the results to the serial monitor:
-  Serial.print("ADC_IN = ");
-  Serial.print(analogRead(ADC_IN));
-  Serial.print("\n");
+  //Serial.print("ADC_IN = ");
+  //Serial.print(analogRead(ADC_IN));
+  //Serial.print("\n");
+  /*
+  if(!con){
+    
+    //establish connection
+    int dev = 0x5A<<1;
+    int data_low = 0;
+    int data_high = 0;
+    int pec = 0;
+    
+    i2c_start_wait(dev+I2C_WRITE);
+    i2c_write(0x07);
+    
+    // read
+    i2c_rep_start(dev+I2C_READ);
+    data_low = i2c_readAck(); //Read 1 byte and then send ack
+    data_high = i2c_readAck(); //Read 1 byte and then send ack
+    pec = i2c_readNak();
+    i2c_stop();
+  }
+  */
+  measureChannelsForInterval();
+  uploadResults();
   delay(1000);
 }
 
